12ed70f59b6f7862e5e603ae2c8057b67a5028ba
Evgeniya <e.polozova@corp.vk.com>
2021-06-15 18:23:25 +0300

    Button: add a11y unit tests

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,6 +1,32 @@
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { baselineComponent } from '../../testing/utils';
 import Button from './Button';
 
+const button = () => screen.getByTestId('custom-btn');
+
 describe('Button', () => {
   baselineComponent(Button);
+
+  it('a11y: hasFocusVisible manages focus visible class properly', () => {
+    const { rerender } = render(<Button data-testid="custom-btn">hasFocusVisible TRUE</Button>);
+    expect(button()).toHaveClass('Tappable--focus-visible-outline');
+
+    rerender(<Button hasFocusVisible={false} data-testid="custom-btn">hasFocusVisible FALSE</Button>);
+    expect(button()).not.toHaveClass('Tappable--focus-visible-outline');
+  });
+
+  it('a11y: custom button has role="button" and tabindex', () => {
+    render(<Button Component="div" data-testid="custom-btn">Кнопка #1</Button>);
+
+    expect(button()).toHaveAttribute('role', 'button');
+    expect(button()).toHaveAttribute('tabindex', '0');
+  });
+
+  it('a11y: custom button gets focused on tab', () => {
+    render(<Button Component="div" data-testid="custom-btn">Кнопка #1</Button>);
+
+    userEvent.tab();
+    expect(button()).toHaveFocus();
+  });
 });
